8e708886e566d191f055e36966dad39198838ad3
sateesh3048 <sateesh.k09@gmail.com>
2017-12-26 17:30:22 +0530

    Update date_data_types.sql

--- date_data_types.sql
+++ date_data_types.sql
@@ -225,3 +225,59 @@ mysql> select datediff(@dt, "2016-01-01") as dates_difference;
 1 row in set (0.00 sec)
 
 
+Sql Query By using above mentioned date functions :-
+=========================================================
+
+select  
+
+NOW() as CURRENT_DATE_TIME, # Get current Date and Time using now() function
+CURDATE() as SYSTEM_DATE, # Get System Date
+DATE(NOW()) as Today_DATE, # Get the current date from now() function using date() function
+
+# Get DAY(), MONTH(), QUARTER(), YEAR() values from given date using below functions
+
+DAY(NOW()) as day, # Get the current day using DAY() function ,
+MONTH(NOW()) as current_month, # Get the current month using MONTH() function,
+QUARTER(NOW()) as current_qurater, # Get the current quarter using QUARTER() function,
+YEAR(NOW()) as current_year, # Get the current year using YEAR() function
+
+# Get Week(), WeekDay(), Weekofyear() from below functions
+
+WEEK(NOW()) as CURRENT_WEEK, # Get Current week number,
+WEEKOFYEAR(NOW()) AS WEEK_OF_YEAR,
+WEEKDAY(NOW()) as WEEK_DAY, # Get current day of week using WEEKDAY function, 
+
+DATE_FORMAT(NOW(), '%m-%d-Y') as USA_DATE, # Convert givent date to mm-dd-yyyy format using DATE_FORMAT function,
+DATEDIFF(NOW(), "2016-12-26") as DATES_DIFFERENCE, # Get the difference between two dates using DATEDIFF function,
+
+# DATE ADD Function to increment dates
+DATE_ADD(NOW(), INTERVAL 1 DAY) as 'One day Later', # add 1 day to current date using DATE_ADD function and INTERVAL 1 day
+DATE_ADD(NOW(), INTERVAL 1 Week) as 'one week later', # Similarly Adding 1 week to current date using INTERVAL 1 week
+DATE_ADD(NOW(), INTERVAL 1 MONTH) as 'One month later',
+
+# Date subtract function to subtract dates
+DATE_SUB(NOW(),  INTERVAL 1 year) as 'One year before', # Get one year before date,
+DATE_SUB(NOW(), INTERVAL 1 month) as 'One month before' \G; # Get one month before date
+
+*************************** 1. row ***************************
+CURRENT_DATE_TIME: 2017-12-26 14:59:48
+      SYSTEM_DATE: 2017-12-26
+       Today_DATE: 2017-12-26
+              day: 26
+    current_month: 12
+  current_qurater: 4
+     current_year: 2017
+     CURRENT_WEEK: 52
+     WEEK_OF_YEAR: 52
+         WEEK_DAY: 1
+         USA_DATE: 12-26-Y
+ DATES_DIFFERENCE: 365
+    One day Later: 2017-12-27 14:59:48
+   one week later: 2018-01-02 14:59:48
+  One month later: 2018-01-26 14:59:48
+  One year before: 2016-12-26 14:59:48
+ One month before: 2017-11-26 14:59:48
+1 row in set (0.00 sec)
+
+
+
